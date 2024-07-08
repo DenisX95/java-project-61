f718ccfce3e81fa5f5c8d78e2caadaae0ae494ff
Klopov <klopov.1995@list.ru>
2024-07-08 20:57:54 +0300

    update App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,8 +1,29 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Brain Games!");
-        Cli.greetTheUser();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter the game number and press Enter.\n"
+            + "1 - Greet\n"
+            + "2 - Even\n"
+            + "0 - Exit");
+
+        var answer = scanner.nextInt();
+        System.out.println("Your choice: " + answer + "\n");
+
+        switch (answer) {
+            case 0:
+                return;
+            case 1:
+                User user = new User();
+                user.greetUser();
+                break;
+            case 2:
+                CheckParityGame.checkParity();
+                break;
+        }
     }
 }
