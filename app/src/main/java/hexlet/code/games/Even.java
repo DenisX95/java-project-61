1c58826398b0dbfc62d80ae14c71f286c84d98eb
Klopov <klopov.1995@list.ru>
2024-07-15 18:44:22 +0300

    rename & update CheckParityGame.java

--- app/src/main/java/hexlet/code/games/Even.java
+++ app/src/main/java/hexlet/code/games/Even.java
@@ -5,42 +5,20 @@ import java.util.Random;
 public class Even {
 
     public static String getMessage() {
-        return "\"Answer 'yes' if the number is even, otherwise answer 'no'.\"";
+        return "Answer 'yes' if the number is even, otherwise answer 'no'.";
     }
 
     public static String getQuestion() {
         Random random = new Random();
         var randomNumber = random.nextInt(101);
-        return randomNumber + "\n"
+        return String.valueOf(randomNumber);
     }
 
     public static String getCorrectAnswer(String question) {
-
-    }
-
-    public static void checkParity () {
-        User user = new User();
-        user.greetUser();
-
-        System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
-
-        while (correctAnswersCount != 3) {
-
-
-            if (answer.equals("yes") && randomNumber % 2 == 0) {
-                correctAnswersCount++;
-                System.out.println("Correct!");
-            } else if(answer.equals("no") && randomNumber % 2 != 0) {
-                correctAnswersCount++;
-                System.out.println("Correct!");
-            } else {
-                var correctAnswer = answer.equals("yes") ? "no" : "yes";
-                System.out.println("'" + answer + "' is wrong answer ;(. Correct answer was '" + correctAnswer + "'.\n"
-                    + "Let's try again, " + user.getUserName() + "!");
-                return;
-            }
-
-            System.out.println("Congratulations, " + user.getUserName() + "!");
+        var randomNumber = Integer.parseInt(question);
+        if (randomNumber % 2 == 0) {
+            return "yes";
         }
+        return "no";
     }
 }
