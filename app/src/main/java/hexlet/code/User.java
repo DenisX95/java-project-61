dd8283e28084808e292eef55bae786989d835370
Klopov <klopov.1995@list.ru>
2024-07-15 18:42:25 +0300

    update User.java

--- app/src/main/java/hexlet/code/User.java
+++ app/src/main/java/hexlet/code/User.java
@@ -3,19 +3,9 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class User {
-    private String userName;
-
-    public User() {
+    public static String getUserName() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome to the Brain Games!\nMay I have your name?");
-        userName = scanner.nextLine();
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void greetUser() {
-        System.out.println("Hello, "+ userName +"!");
+        return scanner.nextLine();
     }
 }
