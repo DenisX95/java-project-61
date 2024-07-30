cdfc389d3c04074f71d63a7c18c7b321e9f844b5
Klopov <klopov.1995@list.ru>
2024-07-30 22:16:25 +0300

    feat (Prime.java): add new file
    new game 'Prime' code

---
+++ app/src/main/java/hexlet/code/games/Prime.java
@@ -0,0 +1,46 @@
+package hexlet.code.games;
+
+import java.util.Random;
+
+public class Prime {
+
+    public static String getMessage() {
+        return "Answer 'yes' if given number is prime. Otherwise answer 'no'";
+    }
+
+    public static String getQuestion() {
+        Random random = new Random();
+        var randomNumber = random.nextInt(100) + 1;
+        return String.valueOf(randomNumber);
+    }
+
+    public static String getCorrectAnswer(String question) {
+        if (isPrime(Integer.parseInt(question))) {
+            return "yes";
+        } else {
+            return "no";
+        }
+    }
+
+    public static boolean isPrime(int number) {
+        if (number <= 1) {
+            return false;
+        }
+
+        if (number == 2) {
+            return true;
+        }
+
+        if (number % 2 == 0) {
+            return false;
+        }
+
+        for (var i = 3; i * i <= number; i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
