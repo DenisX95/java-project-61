bd920f47644ce790f0420693e813e5be0a9bfbe1
Klopov <klopov.1995@list.ru>
2024-07-30 22:17:35 +0300

    feat (App.java): update file
    add new game 'Prime'

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -6,13 +6,15 @@ public class App {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Please enter the game number and press Enter.\n" +
-            "1 - Greet\n" +
-            "2 - Even\n" +
-            "3 - Calc\n" +
-            "4 - GCD\n" +
-            "5 - Progression\n" +
-            "0 - Exit");
+        System.out.println("""
+                Please enter the game number and press Enter.
+                1 - Greet
+                2 - Even
+                3 - Calc
+                4 - GCD
+                5 - Progression
+                6 - Prime
+                0 - Exit""");
 
         var answer = scanner.nextInt();
         System.out.println("Your choice: " + answer + "\n");
