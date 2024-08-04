3a2ba8788780f24ea89136439293af73b4e9299a
Klopov <klopov.1995@list.ru>
2024-08-04 17:33:55 +0300

    fix (Prime.java): update method getQuestion
    Changed the condition in the if block.
    Incorrect value at number == 4 excluded

--- app/src/main/java/hexlet/code/games/Prime.java
+++ app/src/main/java/hexlet/code/games/Prime.java
@@ -28,7 +28,7 @@ public class Prime {
             return false;
         }
 
-        for (int i = 2; i < number / 2; i++) {
+        for (int i = 2; i <= number / 2; i++) {
             if (number % i == 0) {
                 return false;
             }
