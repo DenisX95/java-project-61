ca40d98673a7bbc114d188bf96c4910d0722f56f
Klopov <klopov.1995@list.ru>
2024-08-18 22:16:00 +0300

    refactor (Cli.java): replacing User class with Cli class
    Transferred the functionality of the User class to the Cli class. Then deleted the User class

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Cli {
     public static void greetTheUser() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("May I have your name?");
+        System.out.println("Welcome to the Brain Games!\nMay I have your name?");
         String userName = scanner.nextLine();
         System.out.println("Hello, " + userName + "!");
     }
